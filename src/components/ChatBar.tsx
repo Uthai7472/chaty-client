37d93bc2114c67d5d95c7099e22469a9dd15b134
Uthai7472 <uthai.khantamongkhon.a7p@ap.denso.com>
2024-07-02 21:37:37 +0700

    first commit

--- src/components/ChatBar.tsx
+++ src/components/ChatBar.tsx
@@ -55,6 +55,7 @@ const ChatBar = () => {
       };
 
       if (formData.message) {
+        // const response = await axios.post('https://chaty-server1.onrender.com/api/chat/send', updatedFormData);
         const response = await axios.post('https://chaty-server1.onrender.com/api/chat/send', updatedFormData);
         console.log(response.data);
         console.log('Form Data: ', updatedFormData);
